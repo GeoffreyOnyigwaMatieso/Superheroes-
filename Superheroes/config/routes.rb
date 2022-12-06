a4e7740ce9eee801921a15ec18f72ea33699b03d
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-06 16:13:21 +0300

    Fixed a typo

--- Superheroes/config/routes.rb
+++ Superheroes/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :powers, only: [:index, :show, :update]
-  resources :hero_powers, , only: :create
+  resources :hero_powers, only: :create
   resources :heros, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

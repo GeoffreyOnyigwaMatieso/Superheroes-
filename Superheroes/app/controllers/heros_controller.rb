803a39bb3d97c68e9f41bf8062baf072cf60859b
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-06 15:11:06 +0300

    Set up CRUD for HerosControllers

--- Superheroes/app/controllers/heros_controller.rb
+++ Superheroes/app/controllers/heros_controller.rb
@@ -1,2 +1,14 @@
 class HerosController < ApplicationController
+    def index
+        render json: Hero.all
+      end
+    
+      def show
+        hero = Hero.find_by(id: params[:id])
+        if hero
+        render json: hero, serializer: HerowithpowersSerializer
+        else
+          render json: {error: "Hero not found"}, status: :not_found
+        end
+      end
 end

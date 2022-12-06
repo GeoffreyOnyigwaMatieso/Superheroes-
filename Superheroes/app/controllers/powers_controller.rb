79879243c5fc757891e88da1fff0859a1d2167b1
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-06 15:13:02 +0300

    Setting up controller for powers

--- Superheroes/app/controllers/powers_controller.rb
+++ Superheroes/app/controllers/powers_controller.rb
@@ -1,2 +1,34 @@
 class PowersController < ApplicationController
+    def index
+        render json: Power.all
+      end
+    
+      def show
+        power = find_power
+        if power
+          render json: power
+        else
+          render json: {error: "Power not found"}, status: :not_found
+        end
+      end
+    
+        def update
+          power = find_power
+          if power
+            power.update!(power_params)
+            render json: power
+          else
+            render json: {error: "Power not found"}, status: :not_found
+        end
+      end
+    
+      private
+    
+      def find_power
+        Power.find_by(id: params[:id])
+      end
+    
+      def power_params
+        params.permit(:name, :description)
+      end
 end

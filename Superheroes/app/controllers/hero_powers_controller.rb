2506424230495f6ebcf71fbc40530c7c892f8726
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-06 15:06:40 +0300

    Set CRUD for HeroPowerController

--- Superheroes/app/controllers/hero_powers_controller.rb
+++ Superheroes/app/controllers/hero_powers_controller.rb
@@ -1,2 +1,13 @@
 class HeroPowersController < ApplicationController
+    #Setting our create method 
+  def create
+    heropower = HeroPower.create!(hero_power_params)
+    render json: heropower.hero, serializer: HerowithpowersSerializer
+  end
+
+  private
+
+  def hero_power_params
+    params.permit(:strength, :power_id, :hero_id)
+  end
 end
--- Superheroes/config/routes.rb
+++ Superheroes/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :powers
-  resources :hero_powers
-  resources :heros
+  resources :powers, only: [:index, :show, :update]
+  resources :hero_powers, , only: :create
+  resources :heros, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
